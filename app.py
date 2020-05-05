30f3e3676c57ea6c5cd72126e5fa9f3cee670437
Phony Lou <louqilin555@gmail.com>
2020-05-05 18:18:48 +0800

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+import time
+
+import redis
+import random
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return random.randint(1, 9)
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello from Docker! The latest secret number is {} .\n'.format(count)
