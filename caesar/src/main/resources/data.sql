c1eaa5c07953ec36b81acf784b02a6d1a02bf4be
Phony Lou <louqilin555@gmail.com>
2020-05-05 20:34:09 +0800

    add docker compose

---
+++ caesar/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (name) VALUES ('Kenny');
+INSERT INTO person (name) VALUES ('Nicole');
+INSERT INTO person (name) VALUES ('Jack');
+INSERT INTO person (name) VALUES ('Mary');
+INSERT INTO person (name) VALUES ('David');
