c5be04de2e53dbce65c3760a84f4448728657538
Phony Lou <louqilin555@gmail.com>
2020-05-06 21:22:17 +0800

    Add redis

---
+++ postman/src/main/java/com/postman/service/PostmanService.java
@@ -0,0 +1,21 @@
+package com.postman.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostmanService {
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    public String hello() {
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set("k1", "v1");
+        Object k1 = ops.get("k1");
+        System.out.println(k1);
+
+        return "Hello " + k1;
+    }
+}
